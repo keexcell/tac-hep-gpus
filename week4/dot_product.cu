dc068a1414216aa37590256982335b247d14c9ca
Kayleigh Excell <kexcell@wisc.edu>
2025-10-25 19:15:44 -0500

    Completed HW

--- week4/dot_product.cu
+++ week4/dot_product.cu
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <iostream>
 #include <time.h>
 
 
@@ -23,9 +24,12 @@ const int b = 1;
 
 // CUDA kernel that runs on the GPU
 __global__ void dot_product(const int *A, const int *B, int *C, int N) {
-
-	// FIXME
-	// Use atomicAdd	
+	// Use atomicAdd
+	int idx = threadIdx.x + blockIdx.x * blockDim.x;
+	if (idx < N) {
+		float temp = A[idx]*B[idx];
+		atomicAdd(C,temp);
+	}
 }
 
 
@@ -45,24 +49,41 @@ int main() {
 
 	*h_C = 0;
 
-
+	float size = DSIZE*sizeof(int);
 	// Allocate device memory 
-	
+	cudaMalloc(&d_A, size);
+    	cudaMalloc(&d_B, size);
+    	cudaMalloc(&d_C, sizeof(int));
 	// Check memory allocation for errors
-
-	// Copy the matrices on GPU
+	cudaCheckErrors();
 	
+	// Copy the matrices on GPU
+	cudaMemcpy(d_A, h_A, size, cudaMemcpyHostToDevice);
+	cudaMemcpy(d_B, h_B, size, cudaMemcpyHostToDevice);
 	// Check memory copy for errors
-
+	cudaCheckErrors();
+	
 	// Define block/grid dimentions and launch kernel
+	const int block_size = 32;
+	const int grid_size = DSIZE/block_size;
+	dot_product<<<grid_size, block_size>>>(d_A, d_B, d_C, DSIZE);
 	
 	// Copy results back to host
-	
-    // Check copy for errors
+	cudaMemcpy(h_C, d_C, sizeof(int), cudaMemcpyDeviceToHost);
+	// Check copy for errors
+        cudaCheckErrors();
 
-	// Verify result
+	// Verify result - expect 256 bc DSIZE = 256 and A and B are filled with 1's
+	std::cout << "Expected = 256" << std::endl;
+	std::cout << "C = " << *h_C << std::endl;
 
 	// Free allocated memory
+	free(h_A);
+    	free(h_B);
+    	free(h_C);
+    	cudaFree(d_A);
+    	cudaFree(d_B);
+   	cudaFree(d_C);
 	
 	return 0;
 
